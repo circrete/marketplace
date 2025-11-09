8c0a8e32451b8156d230d39e06ce2567ccf1e99c
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-09 17:30:30 +0100

    feat: SVGIcon like / dislike buttons now have actual actions attached to them

--- components/ElementCard.tsx
+++ components/ElementCard.tsx
@@ -2,20 +2,30 @@ import { useTranslation } from 'react-i18next';
 import Link from './Link';
 import { SVGIcon } from '@/components/SVGIcon';
 import heart from '/assets/icons/heart.svg';
+import brokenHeart from '/assets/icons/heart_broken.svg';
 import cart from '/assets/icons/shopping_cart.svg';
 import { ElementData } from '@/lib/elements';
+import { useCircreteStore } from '@/lib/store';
 
 export const ElementCard: React.FC<{ element: ElementData }> = ({ element }) => {
   const { t } = useTranslation('common');
 
   return (
     <Link href={`/elements/${element.id}`} className="">
-      <div className="flex flex-col items-center w-[200px] h-[300px] rounded-2xl overflow-clip shadow-xl">
+      <div className="flex flex-col items-center w-[calc(min(200px,45svw))] h-[calc(min(300px,60svw))] rounded-2xl overflow-clip shadow-xl">
         <img src={element.src} alt="lamp" className="w-[200px] h-[230px] object-cover" />
-        <div className="my-auto px-4 font-bold flex flex-row justify-between w-full gap-2 shadow-none">
-          <SVGIcon src={heart.src} className="h-4 my-auto" />
-          <h3 className="text-10">{element.name}</h3>
-          <SVGIcon src={cart.src} className="h-4 my-auto" />
+        <div className="font-bold flex flex-row justify-between w-full gap-2 shadow-none">
+          <SVGIcon
+            onClick={() => useCircreteStore.getState().addLiked(element.id)}
+            src={heart.src}
+            className="h-4 my-auto"
+          />
+          <p className="text-10">{element.name}</p>
+          <SVGIcon
+            onClick={() => useCircreteStore.getState().addDisliked(element.id)}
+            src={brokenHeart.src}
+            className="h-4 my-auto"
+          />
         </div>
       </div>
     </Link>
--- components/SVGIcon.tsx
+++ components/SVGIcon.tsx
@@ -1,3 +1,18 @@
-export const SVGIcon: React.FC<{ src: string; className?: string }> = ({ src, className }) => (
-  <img src={src} alt="logo" className={className ?? 'h-8'} />
+const baseStyle = 'h-8';
+
+export const SVGIcon: React.FC<{ src: string; className?: string; onClick?: () => void }> = ({
+  src,
+  className,
+  onClick
+}) => (
+  <img
+    src={src}
+    alt="logo"
+    className={className ?? onClick ? baseStyle + ' cursor-pointer' : baseStyle}
+    onClick={(e) => {
+      console.log('clicked');
+      e.preventDefault();
+      onClick?.();
+    }}
+  />
 );
