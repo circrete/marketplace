b252471d63f2432bad572a20725cd627c205151c
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-09 20:11:23 +0100

    fix: onClick only when method is given!

--- components/SVGIcon.tsx
+++ components/SVGIcon.tsx
@@ -1,18 +1,18 @@
 const baseStyle = 'h-8';
 
-export const SVGIcon: React.FC<{ src: string; className?: string; onClick?: () => void }> = ({
-  src,
-  className,
-  onClick
-}) => (
+export const SVGIcon: React.FC<{ src: string; className?: string; onClick?: () => void }> = ({ src, className, onClick }) => (
   <img
     src={src}
     alt="logo"
     className={className ?? onClick ? baseStyle + ' cursor-pointer' : baseStyle}
-    onClick={(e) => {
-      console.log('clicked');
-      e.preventDefault();
-      onClick?.();
-    }}
+    onClick={
+      onClick
+        ? (e) => {
+            console.log('clicked');
+            e.preventDefault();
+            onClick?.();
+          }
+        : undefined
+    }
   />
 );
