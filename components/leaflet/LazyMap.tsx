38c10e14e1f4393c60f591a34ab984156e9a2d2c
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-09 17:13:53 +0100

    fix: lazy-map loading p also fills container

--- components/leaflet/LazyMap.tsx
+++ components/leaflet/LazyMap.tsx
@@ -5,7 +5,7 @@ import dynamic from 'next/dynamic';
 
 const LazyMap = dynamic(() => import('@/components/leaflet/Map'), {
   ssr: false,
-  loading: () => <p>Loading...</p>
+  loading: () => <p className="w-full h-full flex items-center justify-center">Loading...</p>
 });
 
 export const Map: React.FC<{ elements: ElementData[]; className: string }> = ({ elements, className }) => (
